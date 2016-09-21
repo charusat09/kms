f81967289625c278ec80456e7b18489f438a6de4
prakash <prakash.chaudhary@bacancytechnology.com>
2016-09-21 11:28:28 +0530

    model leval rspec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -100,7 +100,7 @@ RSpec.configure do |config|
   # as the one that triggered the failure.
   Kernel.srand config.seed
 =end
-ENV["RAILS_ENV"] ||= 'development'
+ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 # require 'rspec/rails'
 # require 'rspec/autorun'
