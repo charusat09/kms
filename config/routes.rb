2e53043902bbc1e282bddf18b173caf99f41f6a4
prakash <prakash.chaudhary@bacancytechnology.com>
2016-09-21 14:53:49 +0530

    redirect root path

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root :to => 'home#index'
   devise_for :users, controllers: {registrations: "users/registrations"}
+  get "*path" => redirect("/")
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
