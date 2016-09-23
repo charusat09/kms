5693ba2778a60d51804249f8b9c2806bafe5ace4
Hardik Upadhyay <hardik.upadhyay@bacancytechnology.com>
2016-09-23 15:43:28 +0530

    Category CRUD

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -1,11 +1,27 @@
 class Api::V1::BaseController < ApplicationController
- # acts_as_token_authentication_handler_for User
-  def current_user
+  protect_from_forgery with: :null_session
+
+  def render_success(message,data)
+    render json: {
+      status: 'success',
+      message: message,
+      data:   data
+    } 
+  end
+  def render_error(errors,data)
+    render json: {
+      status: 'error',
+      data:   data,
+      errors: errors
+    }, status: 422
+  end 
+
+  def authorize_user!
     if params[:auth_token].present?  
-      @token = Authentication.find_by(auth_token:: params[:auth_token])
+      @token = Authentication.find_by(auth_token: params[:auth_token])
       if @token.present?
-        @user = User.find(@token.user_id)
-        unless @user.present? 
+        @current_user = User.find(@token.user_id)
+        unless @current_user.present? 
           render_json({:errors => "No user found with this #{params[:auth_hash]} auth token", :status => 404}.to_json)
         end
       else
@@ -13,6 +29,4 @@ class Api::V1::BaseController < ApplicationController
       end  
     end
   end
-
 end
-
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
    namespace :api, defaults: {format: 'json'} do
     namespace :v1 do
       devise_for :users, controllers: {registrations: "api/v1/registrations"}
+      resources :categories
     end
   end
 end
