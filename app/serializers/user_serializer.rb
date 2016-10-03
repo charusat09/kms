0d3e7efa484d8fc04341d4b09a20206059817150
prakash <prakash.chaudhary@bacancytechnology.com>
2016-10-03 10:58:38 +0530

    return auth_token for user

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -2,7 +2,7 @@ class UserSerializer < ActiveModel::Serializer
   attributes :id, :name, :email, :auth_token
   def auth_token
     object.authentications.each do |user|
-      user.auth_token
+      return user.auth_token
     end
   end
 end
