7e8adbaa1a95ab33887cd4562a790818f3c55226
Mayurkumar Patel <mayurt20@gmail.com>
2016-10-03 14:26:27 +0530

    resolves articles index route issue

--- app/controllers/api/v1/articles_controller.rb
+++ app/controllers/api/v1/articles_controller.rb
@@ -4,6 +4,7 @@ class Api::V1::ArticlesController < Api::V1::BaseController
   
   def index
     @articles = Article.all
+    render_success("articles",@articles)
   end
 
   def create
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
    namespace :api, defaults: {format: 'json'} do
     namespace :v1 do
       devise_for :users, controllers: {registrations: "api/v1/registrations",sessions: "api/v1/sessions"}
-      resource :articles
+      resources :articles
       resources :categories
     end
   end
